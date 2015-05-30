976cca7cd1992de1c909b2663e276639821aa109
light-flight <yurievg@mail.ru>
2015-05-30 03:03:39 +0300

    optimization
    + путем упрощения кода удалось повысить производительность в 60 раз!
    + налажен мониторинг статистики скорости выполнения операций
    
    - тщательная проверка кода не производилась
    - по теоретическим данным возможна обработка файлов размером до 4,5
    гигабайт

--- Lab_13/main.c
+++ Lab_13/main.c
@@ -34,28 +34,22 @@ typedef struct _binaryTree* tree;
 void fromTextToTree(char*, tree*);
 void addToTree(tree*, tree*);
 void print(tree);
-void statistic(double);
+void statistic(double, char*, long unsigned int);
+long unsigned int file_size(char*);
 
 int main() {
 	clock_t start, finish;
 	char filename[] = "tree.txt";
 	tree root = NULL;
-	struct stat  file_stat;
-
-	if (stat(filename, &file_stat) == -1) {
-		puts("Can not get file statistics!");
-	}
-	else {
-		printf("The size of \"%s\" is %u bytes.\n", filename, file_stat.st_size);
-	}
 
 	start = clock();
 	fromTextToTree(filename, &root);
 	finish = clock();
-	printf("Simple concordance task was accomplished in %f sec.\n", ((double)(finish - start)) / CLOCKS_PER_SEC);
+	printf("Task was accomplished in %f sec.\n", ((double)(finish - start)) / CLOCKS_PER_SEC);
 
-	statistic(((double)(finish - start)) / CLOCKS_PER_SEC);
+	statistic(((double)(finish - start)) / CLOCKS_PER_SEC, filename, file_size(filename));
 	print(root);
+
 	system("PAUSE");
 }
 
@@ -73,31 +67,21 @@ void fromTextToTree(char *filename, tree *root) {
 		if ((character != ' ') && (character != '.') && (character != '\n') &&
 		(character != ',') && (character != '\0') &&
 		(character != '!') && (character != '?')) { // если это буква
-
-			if (current == NULL) { // если это новое слово
-				if (!(current = (tree)malloc(sizeof(struct _binaryTree)))) {
+			if (!current) { // если это новое слово
+				if (!(current = (tree)malloc(sizeof(struct _binaryTree)))) { // выделяем для него память
 					puts("Can not allocate memory!");
 					return;
 				}
-				current->repeat = 1;
-				i = 0;
+				current->repeat = 1; // инициализируем количество повторений этого слова
+				i = 0; // инициализируем счетчик для массива букв
 			}
-
-			current->word[i++] = character;
-
-			if ((symbol = fgetc(stream)) == EOF) { // необходимо проинспектировать следующий элемент 
-				return;
-			}
-			else {
-				character = symbol;
-				if ((character == ' ') || (character == '.') || (character == '\n') ||
-				(character == ',') || (character == '\0') ||
-				(character == '!') || (character == '?')) { // если слово закончилось
-					current->word[i] = '\0';
-					addToTree(&current, root);
-					current = NULL;
-				}
-				fseek(stream, -1, SEEK_CUR); // возвращаем каретку назад на одну позицию
+			current->word[i++] = character; // заносим букву в массив
+		}
+		else {
+			if (current) { // если слово закончилось
+				current->word[i] = '\0';
+				addToTree(&current, root);
+				current = NULL;
 			}
 		}
 	}
@@ -130,16 +114,36 @@ void print(tree root) {
 	}
 }
 
-void statistic(double result) {
+void statistic(double result, char *filename, long unsigned int file_size) {
 	char speed_comment[80];
 	time_t t = time(NULL);
 	struct tm* aTm = localtime(&t);
 	FILE *timing;
+
+	if (file_size == 0) {
+		puts("File is empty!");
+		return;
+	}
+
+	printf("The size of \"%s\" is %u bytes.\n", filename, file_size);
+
 	if (!(timing = fopen("timing.txt", "a"))) {
 		puts("Can not open file \"timing.txt\"!");
 		return;
 	}
 	puts("Add a comment about optimisation:");
 	gets(speed_comment);
-	fprintf(timing, "[%02d:%02d:%02d] --- %f | %s\n", aTm->tm_hour, aTm->tm_min, aTm->tm_sec, result, speed_comment);
+	fprintf(timing, "[%02d:%02d:%02d] \t | \t \"%s\" [size: %u Mb] \t | \t %f sec \t | \t %s\n",
+		aTm->tm_hour, aTm->tm_min, aTm->tm_sec, filename, file_size / 1000 / 1000, result, speed_comment);
+}
+
+long unsigned int file_size(char* filename) {
+	struct stat file_stat;
+
+	if (stat(filename, &file_stat) == -1) {
+		puts("Can not get file statistics!");
+		return 0;
+	}
+	else
+		return file_stat.st_size;
 }
